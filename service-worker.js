d41b940710bc5f088d353c37f1743fe3c81501bc
Washingtonbarbalho <137066669+Washingtonbarbalho@users.noreply.github.com>
2025-01-03 22:14:20 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "calc-salario-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/style.css",
+  "/script.js", // Substitua pelos nomes reais dos arquivos
+  "/icon-192x192.png",
+  "/icon-512x512.png"
+];
+
+// Instalar o Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Ativar o Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Buscar os recursos no cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
